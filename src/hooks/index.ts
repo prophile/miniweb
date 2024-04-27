08ca5e0f7ba4a9fa24b749446d246a17509582b2
Alistair Lynn <arplynn@gmail.com>
2024-04-27 00:19:58 +0000

    This is all `export`

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,32 +1,15 @@
-import { useState } from "./useState";
-import { useReducer } from "./useReducer";
-import { useRef } from "./useRef";
-import { useMemo } from "./useMemo";
-import { useCallback } from "./useCallback";
-import { useEffect } from "./useEffect";
-import { useLayoutEffect } from "./useLayoutEffect";
-import { useInsertionEffect } from "./useInsertionEffect";
-import { useImperativeHandle } from "./useImperativeHandle";
-import { useDebugValue } from "./useDebugValue";
-import { useTransition } from "./useTransition";
-import { useSyncExternalStore } from "./useSyncExternalStore";
-import { useOptimistic } from "./useOptimistic";
-import { useDeferredValue } from "./useDeferredValue";
-import { useId } from "./useId";
-export {
-  useState,
-  useReducer,
-  useRef,
-  useMemo,
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useInsertionEffect,
-  useImperativeHandle,
-  useDebugValue,
-  useTransition,
-  useSyncExternalStore,
-  useOptimistic,
-  useDeferredValue,
-  useId,
-};
+export { useState } from "./useState";
+export { useReducer } from "./useReducer";
+export { useRef } from "./useRef";
+export { useMemo } from "./useMemo";
+export { useCallback } from "./useCallback";
+export { useEffect } from "./useEffect";
+export { useLayoutEffect } from "./useLayoutEffect";
+export { useInsertionEffect } from "./useInsertionEffect";
+export { useImperativeHandle } from "./useImperativeHandle";
+export { useDebugValue } from "./useDebugValue";
+export { useTransition } from "./useTransition";
+export { useSyncExternalStore } from "./useSyncExternalStore";
+export { useOptimistic } from "./useOptimistic";
+export { useDeferredValue } from "./useDeferredValue";
+export { useId } from "./useId";
